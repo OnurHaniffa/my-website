4c451ade7fd447a09ddd35ce2f358c9434d0d14d
OnurHaniffa <mronursa@gmail.com>
2025-12-24 18:27:34 +0300

    Use logo.png as favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo.png';
 	import { Header, Footer } from '$lib/components/layout';
 	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
